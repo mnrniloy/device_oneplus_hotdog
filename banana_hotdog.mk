a39c63c1a1421e9fd77381b16a4a96a1164ad87f
mnrniloy <mnrniloy0@gmail.com>
2024-03-11 00:26:17 +0600

    hotdog: Bringup Banana

--- banana_hotdog.mk
+++ banana_hotdog.mk
@@ -11,11 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hotdog device
 $(call inherit-product, device/oneplus/hotdog/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common Bananadroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
+
+# Supported Device Flags.
+BANANA_MAINTAINER := NILOY
+WITH_GAPPS := true
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGER_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_hotdog
+PRODUCT_NAME := banana_hotdog
 PRODUCT_DEVICE := hotdog
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := HD1917
@@ -29,9 +39,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Boot animation
-scr_resolution := 1440
-TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Build info
 BUILD_FINGERPRINT := "OnePlus/OnePlus7TPro/OnePlus7TPro:12/SKQ1.211113.001/Q.202303230244:user/release-keys"
